3c738f836ae3313a90006e794aacf04f3afeac1c
mcalheiro <mauriciocalheiro95@gmail.com>
2024-02-14 17:05:36 +0000

    Add login/logout features

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.views.generic.list import ListView
@@ -6,32 +8,45 @@ from django.urls import reverse_lazy
 from .models import Task
 
 
-class TaskList(ListView):
+class CustomLoginView(LoginView):
+    template_name = "base/login.html"
+    fields = "__all__"
+    redirect_authenticated_user = True
+    
+    def get_success_url(self):
+        return reverse_lazy("tasks")
+
+
+class TaskList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = "tasks"
-    template_name = "base/task_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["tasks"] = context["tasks"].filter(user=self.request.user)
+        context["count"] = context["tasks"].filter(complete=False).count()
+        return context
 
 
-class TaskDetail(DetailView):
+class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
     context_object_name = "task"
     template_name = "base/task.html"
 
 
-class TaskCreate(CreateView):
-    model = Task
-    fields = "__all__"
-    success_url = reverse_lazy("tasks")
-    template_name = "base/task_form.html"
-
-
-class TaskUpdate(UpdateView):
+class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
     fields = "__all__"
     success_url = reverse_lazy("tasks")
 
 
-class TaskDelete(DeleteView):
+class TaskUpdate(LoginRequiredMixin, UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("tasks")
+
+
+class TaskDelete(LoginRequiredMixin, DeleteView):
     model = Task
     success_url = reverse_lazy("tasks")
     context_object_name = 'task'
