d00d4f6434efcebcf1a9ae144d1d9ba2bd4188ee
mcalheiro <mauriciocalheiro95@gmail.com>
2024-02-14 13:29:19 +0000

    Change task view to listview

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic.list import ListView
+from .models import Task
 
-def taskList(request):
-    return HttpResponse("To do list")
+class TaskList(ListView):
+    model = Task
