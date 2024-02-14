bec14e83d050090931fb619dc3a37f09c19e997e
mcalheiro <mauriciocalheiro95@gmail.com>
2024-02-14 18:30:55 +0000

    Hotfix on view.py

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -36,7 +36,7 @@ class RegisterPage(FormView):
     def get(self, *args, **kwargs):
         if self.request.user.is_authenticated:
             return redirect("tasks")
-        return super(RegisterPage, self).get(**args, **kwargs)
+        return super(RegisterPage, self).get(*args, **kwargs)
 
 
 class TaskList(LoginRequiredMixin, ListView):
